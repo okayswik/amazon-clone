448859b6f298b4052b5d555532cd143acf10e5e3
Swikriti Neupane <okayswik@gmail.com>
2021-02-22 15:38:40 +0545

    Amazon-clone getting started

---
+++ src/Header.js
@@ -0,0 +1,46 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        />
+      </Link>
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+        {/* {logo} */}
+      </div>
+
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionlineOne">Hello Guest</span>
+          <span className="header__optionlineTwo">Sign In</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionlineOne">Returns</span>
+          <span className="header__optionlineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionlineOne">Your</span>
+          <span className="header__optionlineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionlineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCV28_kRddxgNrG7Ur0pdoG3bAI0AVpP2o",
+    authDomain: "clone-844aa.firebaseapp.com",
+    projectId: "clone-844aa",
+    storageBucket: "clone-844aa.appspot.com",
+    messagingSenderId: "478538780156",
+    appId: "1:478538780156:web:9875e699a778b80eb0f679",
+    measurementId: "G-6ET7S4R12M"
+  };
